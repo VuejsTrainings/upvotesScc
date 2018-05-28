46d1da8778ef56f9e88233fba53b98f18d56ac3d
Eric Guyader <ericguyader@yahoo.fr>
2018-05-28 12:13:29 +0200

    Ajoute la liste des propositions en JS

---
+++ js/propositions.js
@@ -0,0 +1,57 @@
+const propositions = [
+    {
+        id:1,
+        titre:'Avoir des week-ends de 3 jours',
+        description:'Allonger le week-end et l\'étendre aussi au lundi pour tout le personnel de l\'entreprise.',
+        votes:30,
+        image:'img/beach.jpg',
+        auteur : {
+            nom:'Lay Zee',
+            image:'img/avatars/lazy.jpg'
+        }
+    },
+    {
+        id:2,
+        titre:'Faire financer par le CE un voyage aux Caraïbes',
+        description:'Faire payer par le CE pour tout le personnel de SCC un voyage de 15 jours (au moins) aux Caraïbes.',
+        votes:34,
+        image:'img/caraibes.jpg',
+        auteur : {
+            nom:'Jack Sparrow',
+            image:'img/avatars/jspar.jpg'
+        }
+    },
+    {
+        id:3,
+        titre:'Désigner Zinedine Zidane sélectionneur de l\'équipe de France',
+        description:'A la fin de la Coupe du Monde, et quelque soit le résultat de l\'équipe de France, installer Zizou à la tête de la sélection nationale.',
+        votes:35,
+        image:'img/zz.jpg',
+        auteur : {
+            nom:'KB Madrid',
+            image:'img/avatars/benz.jpg'
+        }
+    },
+    {
+        id:4,
+        titre:'Faire en sorte qu\'il fasse beau toute l\'année',
+        description:'Interdire les nuages et la pluie.',
+        votes:25,
+        image:'img/sun.jpg',
+        auteur : {
+            nom:'Tan Ed',
+            image:'img/avatars/taned.jpg'
+        }
+    },
+    {
+        id:0,
+        titre:'',
+        description:'',
+        votes:0,
+        image:'',
+        auteur : {
+            nom:'',
+            image:''
+        }
+    },
+];
